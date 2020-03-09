d146327c4e5b03617b7af0531411dd5080e43ecc
javitocor <56809963+javitocor@users.noreply.github.com>
2020-03-09 14:56:08 +0100

    Fixed all methods, tested with examplres

--- enumerable.rb
+++ enumerable.rb
@@ -8,7 +8,7 @@ module Enumerable
   end
   
   def my_each_with_index
-    return Enumerator if !block_given?
+    return to_enum (__method__) if !block_given?
       idx = 0
       for x in self
         yield(x, idx)
@@ -34,11 +34,14 @@ module Enumerable
       self.my_each { |x| return false if yield(x) == false }
       true
     elsif arg.class == Regexp
-      self.my_each { |x| return false if arg.match(x) == false }
+      self.my_each { |x| return false if arg.match(x) == nil }
       true
-    else
+    elsif arg.class <= Numeric || arg.class <= String
       self.my_each { |x| return false if x != arg }
-      true
+      true  
+    else
+      self.my_each { |x| return false if (x.is_a? arg) == false }
+      true      
     end
   end
 
@@ -49,10 +52,12 @@ module Enumerable
       my_each { |x| return true if yield(x) == true }
       false
     elsif arg.class == Regexp
-      my_each { |x| return true if arg.match(x) == true }
+      my_each { |x| return true if arg.match(x) != nil }
       false
-    else
-      my_each { |x| return true if x == arg }
+    elsif arg.class <= Numeric || arg.class <= String
+      my_each { |x| return true if x === arg }
+      false      
+    else my_each { |x| return true if x.class <= arg }
       false
     end
     
@@ -65,11 +70,14 @@ module Enumerable
       my_each { |x| return false if yield(x) == true }
       true
     elsif arg.class == Regexp
-      my_each { |x| return false if arg.match(x) == true }
+      my_each { |x| return false if arg.match(x) != nil }
+      true
+    elsif arg.class <= Numeric || arg.class <= String
+      my_each { |x| return false if x === arg }
       true
     else
-      my_each { |x| return false if x == arg }
-      true
+      my_each { |x| return false if x.class <= arg }
+      true      
     end
     
   end
@@ -85,7 +93,7 @@ module Enumerable
         number += 1 if x == par
       end
     else
-      number = length
+      number = self.length
     end
     number
   end
@@ -106,24 +114,126 @@ module Enumerable
   end
 
   def my_inject(par = nil)
-    if par.nil? 
-      array = self.to_a     
+    array = self.to_a
+    if par.nil?           
       acc = array[0]
       for x in array[1...array.length]
         acc = yield(acc, x)
       end
-      acc
     else
-      array = self.to_a
       acc = par
       array.my_each do |x| 
         acc = yield(acc, x)
       end
-      acc
     end
+    acc
   end
 end
 
 def multiply_els(array)
   array.my_inject { |product, n| product * n }
 end
+
+puts 'my_each'
+
+[1, 2, 3, 4, 'hi'].my_each do |x|
+  puts x
+end
+puts [2,4,7,11].my_each #<Enumerator: [2, 4, 7, 11]:my_each>
+
+puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
+puts 'my_each_with_index'
+
+[1, 2, 3, 4, 'hi'].my_each_with_index { |value, index| puts "#{value} => #{index}" }
+puts [2,4,7,11].my_each_with_index  #<Enumerator: [2, 4, 7, 11]:my_each_with_index
+
+puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
+puts 'my_select'
+
+result = [1, 2, 3, 4, 5, 6].select(&:even?) #=> [2, 4, 6]
+puts result
+block = proc { |num| num = 11 }
+puts [2,4,7,11].my_select(&block) #=> [11]
+puts [2,4,7,11].my_select  #<Enumerator: [2, 4, 7, 11]:my_select>
+
+puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
+puts 'my_all?'
+
+puts %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
+puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+puts %w[ant bear cat].my_all?(/t/) #=> false
+puts [1, 2i, 3.14].my_all?(Numeric) #=> true
+puts [nil, true, 99].my_all? #=> false
+puts [].my_all? #=> true
+puts [nil, false, true, []].my_all? #=> false
+puts [1, 2.5, 'a', 9].my_all?(Integer) #=> false
+puts %w[dog door rod blade].my_all?(/d/) #=> true
+puts [3,4,7,11].my_all?(3) #=> false
+
+puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
+puts 'my_any??'
+
+puts %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
+puts %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
+puts %w[ant bear cat].my_any?(/d/) #=> false
+puts [nil, true, 99].my_any?(Integer) #=> true
+puts [nil, true, 99].my_any? #=> true
+puts [].my_any? #=> false
+puts [nil, false, true, []].my_any? #=> true
+puts [1, 2.5, 'a', 9].my_any?(Integer) #=> true
+puts %w[dog door rod blade].my_any?(/d/) #=> true
+puts [3,4,7,11].my_any?(3) #=> true
+
+puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
+puts 'my_none?'
+
+puts %w[ant bear cat].my_none? { |word| word.length == 5 } #=> true
+puts %w[ant bear cat].my_none? { |word| word.length >= 4 } #=> false
+puts %w[ant bear cat].my_none?(/d/) #=> true
+puts [1, 3.14, 42].my_none?(Float) #=> false
+puts [].my_none? #=> true
+puts [nil].my_none? #=> true
+puts [nil, false].my_none? #=> true
+puts [nil, false, true].my_none? #=> false
+puts [nil, false, true, []].my_none? #=> false
+puts [1, 2.5, 'a', 9].my_none?(Integer) #=> false
+puts %w[dog door rod blade].my_none?(/d/) #=> false
+puts [3,4,7,11].my_none?(3) #=> false
+
+puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
+puts 'my_count'
+
+ary = [1, 2, 4, 2]
+puts ary.my_count #=> 4
+puts ary.my_count(2) #=> 2
+puts ary.my_count(&:even?) #=> 3
+
+puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
+puts 'my_map'
+
+puts (1..4).my_map { |i| i * i } #=> [1, 4, 9, 16]
+puts (1..4).my_map { 'dog' } #=> ["dog", "dog", "dog", "dog"]
+puts %w[a b c].my_map(&:upcase) #=> ["A", "B", "C"]
+puts %w[a b c].my_map(&:class) #=> [String, String, String]
+puts [2,4,7,11].my_map #<Enumerator: [2, 4, 7, 11]:my_map
+my_proc = Proc.new {|num| num > 10 }
+puts [18, 22, 5, 6] .my_map(my_proc) {|num| num < 10 } # true true false false
+
+puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
+puts 'my_inject'
+
+longest = %w[cat sheep bear].my_inject do |memo, word|
+  memo.length > word.length ? memo : word
+end
+
+puts longest #=> "sheep"
+
+puts (5..10).my_inject { |sum, n| sum + n } #=> 45
+puts (5..10).my_inject(2) { |sum, n| sum + n } #=> 47
+puts (1..5).my_inject(4) { |prod, n| prod * n } #=> 480
+
+
+puts '-*-*-*-*-*-*-*-*-*-*-*-*-'
+puts 'multiply_els'
+
+puts multiply_els([2, 4, 5]) #=> 40
